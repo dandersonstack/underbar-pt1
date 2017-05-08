b055792ad253507a910feee51b335153a6d19b82
dandersonstack <danieltanderson@berkeley.edu>
2017-05-07 23:01:25 -0700

    added tests for pluck function

--- test/pluck.test.js
+++ test/pluck.test.js
@@ -11,4 +11,24 @@ describe('pluck()', () => {
     expect(result).toEqual([12, 999, 14]);
   });
 
+  it('given an array of objects, can return an array of new objects', () => {
+    const people = [
+      { obj : { name: 'Harriet', age: 12} },
+      { obj : { name: 'Lazarus', age: 999} },
+      { obj : { name: 'Bethany', age: 14} }
+    ];
+    const result = _.pluck(people, 'obj');
+    expect(result).toEqual([{ name: 'Harriet', age: 12} , { name: 'Lazarus', age: 999}, { name: 'Bethany', age: 14} ]);
+  });
+
+  it('returns an array where any object missing an age will have an undefined marker', () => {
+    const people = [
+      { name: 'Harriet', age: 12},
+      { name: 'Lazarus'},
+      { name: 'Bethany', age: 14}
+    ];
+    const result = _.pluck(people, 'age');
+    expect(result).toEqual([12, undefined, 14]);
+  });
+
 });
