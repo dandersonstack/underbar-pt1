945004418f29ab9e4fd192d1e7740f6dbdad6a68
dandersonstack <danieltanderson@berkeley.edu>
2017-05-07 23:01:13 -0700

    added tests for map function

--- test/map.test.js
+++ test/map.test.js
@@ -6,4 +6,11 @@ describe('map()', () => {
     const mappedArr = _.map(arr, (el) => el * el);
     expect(mappedArr).toEqual([1, 4, 9, 16, 25]);
   });
+
+   it('maps every number in an array to that number plus 1', () => {
+    const arr = [1, 2, 3, 4, 5];
+    const mappedArr = _.map(arr, (el) => el +1);
+    expect(mappedArr).toEqual([2, 3, 4, 5, 6]);
+  });
+
 });
