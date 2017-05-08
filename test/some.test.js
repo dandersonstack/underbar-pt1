0e50f8b2a39a8078238e8515819f4066c5336bbe
dandersonstack <danieltanderson@berkeley.edu>
2017-05-07 23:03:36 -0700

    added tests for some function

--- test/some.test.js
+++ test/some.test.js
@@ -6,6 +6,31 @@ describe('some()', () => {
     expect(_.some(nums, num => num % 2 === 1)).toBe(true);
   });
 
+  it('returns true if no callback is passed in', () => {
+    const nums = [2, 4, 5, 6];
+    expect(_.some(nums)).toBe(true);
+  });
+
+  it('returns true if an object has 1 odd number', () => {
+    const nums = [2, 4, 5, 6];
+    const arrayLikeObj = {
+      key1: 'a',
+      key2: 3,
+      key3: 'c'
+    };
+    expect(_.some(nums, num => num % 2 === 1)).toBe(true);
+  });
+
+  it('returns false if an object has 1 odd number', () => {
+    const nums = [2, 4, 5, 6];
+    const arrayLikeObj = {
+      key1: 'a',
+      key2: 'b',
+      key3: 'c'
+    };
+    expect(_.some(nums, num => num % 2 === 1)).toBe(true);
+  });
+
   it('returns false if no number is odd', () => {
     const nums = [2, 4, 6, 8];
     expect(_.some(nums, num => num % 2 === 1)).toBe(false);
