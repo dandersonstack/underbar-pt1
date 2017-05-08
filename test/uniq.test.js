f674cad82971accd7f853e96903a9ffda7b189b2
dandersonstack <danieltanderson@berkeley.edu>
2017-05-07 23:03:47 -0700

    added tests for uniq function

--- test/uniq.test.js
+++ test/uniq.test.js
@@ -5,4 +5,15 @@ describe('uniq()', () => {
     const nums = [2, 4, 5, 5, 7, 4, 10, 2];
     expect(_.uniq(nums)).toEqual([2, 4, 5, 7, 10]);
   });
+
+  it('returns the exact array with no duplicates', () => {
+    const nums = [2, 4, 5, 7, 10, 2];
+    expect(_.uniq(nums)).toEqual([2, 4, 5, 7, 10]);
+  });
+
+  it('returns an array with one null value, when recieving an array of nulls', () => {
+    const nums = [null, null, null, null, null];
+    expect(_.uniq(nums)).toEqual([null]);
+  });
+
 });
